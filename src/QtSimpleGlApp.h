d579c369321249d450eb8895b9c3e665e65840e9
Martin Felis <martin@silef.de>
2012-01-04 13:24:45 +0100

    initial commit

---
+++ src/QtSimpleGlApp.h
@@ -0,0 +1,18 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QTimer>
+#include "ui_MainWindow.h"
+
+class SimpleQtGlApp : public QMainWindow, public Ui::MainWindow
+{
+    Q_OBJECT
+ 
+public:
+    SimpleQtGlApp(QWidget *parent = 0);
+
+protected:
+		QTimer *timer;
+};
+ 
+#endif
---
+++ src/qtmain.cc
@@ -0,0 +1,17 @@
+#include <QApplication>
+
+#include "SimpleQtGlApp.h"
+#include "glwidget.h"
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	QApplication app(argc, argv);
+	SimpleQtGlApp *main_window = new SimpleQtGlApp;
+
+	main_window->show();
+	return app.exec();
+}
