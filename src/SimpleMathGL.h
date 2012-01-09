e1c2e59cc7ce9d3ba9b8fc112524bbe9b0642dd8
martin.felis <none@none>
2012-01-09 19:41:59 +0100

    working on quaternions (yay)

--- src/SimpleMathGL.h
+++ src/SimpleMathGL.h
@@ -4,10 +4,10 @@
 #include "SimpleMath.h"
 #include <cmath>
 
-SimpleMath::Fixed::Matrix<float, 4, 4> smRotate (float rot_deg, float x, float y, float z) {
+inline Matrix44f smRotate (float rot_deg, float x, float y, float z) {
 	float c = cosf (rot_deg * M_PI / 180.f);
 	float s = sinf (rot_deg * M_PI / 180.f);
-	return SimpleMath::Fixed::Matrix<float, 4, 4> (
+	return Matrix44f (
 			x * x * (1.0f - c) + c,
 			y * x * (1.0f - c) + z * s,
 			x * z * (1.0f - c) - y * s,
@@ -27,8 +27,8 @@ SimpleMath::Fixed::Matrix<float, 4, 4> smRotate (float rot_deg, float x, float y
 			);
 }
 
-SimpleMath::Fixed::Matrix<float, 4, 4> smTranslate (float x, float y, float z) {
-	return SimpleMath::Fixed::Matrix<float, 4, 4> (
+inline Matrix44f smTranslate (float x, float y, float z) {
+	return Matrix44f (
 			1.f, 0.f, 0.f, 0.f,
 			0.f, 1.f, 0.f, 0.f,
 			0.f, 0.f, 1.f, 0.f,
@@ -36,8 +36,8 @@ SimpleMath::Fixed::Matrix<float, 4, 4> smTranslate (float x, float y, float z) {
 			);
 }
 
-SimpleMath::Fixed::Matrix<float, 4, 4> smScale (float x, float y, float z) {
-	return SimpleMath::Fixed::Matrix<float, 4, 4> (
+inline Matrix44f smScale (float x, float y, float z) {
+	return Matrix44f (
 			  x, 0.f, 0.f, 0.f,
 			0.f,   y, 0.f, 0.f,
 			0.f, 0.f,   z, 0.f,
@@ -45,6 +45,69 @@ SimpleMath::Fixed::Matrix<float, 4, 4> smScale (float x, float y, float z) {
 			);
 }
 
+/** Quaternion 
+ *
+ * order: x,y,z,w
+ */
+class smQuaternion : public Vector4f {
+	public:
+		smQuaternion (float x, float y, float z, float w):
+			Vector4f (x, y, z, w)
+		{}
+		smQuaternion operator* (const smQuaternion &quat) const {
+			// we basically want to do a
+			//   quat * this
+
+			Vector3f v1 (quat[0], quat[1], quat[2]);
+			Vector3f v2 ( (*this)[0], (*this)[1], (*this)[2]);
+			float w1 = quat[3];
+			float w2 = (*this)[3];
+
+			Vector3f v = w1 * v2 + w2 * v1 + v1.cross(v2);
+
+			return smQuaternion (
+					v[0], v[1], v[2],
+					w1 * w2 - v1.transpose() * v2
+					);
+		}
+
+		static smQuaternion fromGLRotate (float angle, float x, float y, float z) {
+			float st = sin (angle * M_PI / 360.f);
+			return smQuaternion (
+						st * x,
+						st * y,
+						st * z,
+						cos (angle * M_PI / 360.f)
+						);
+		}
+
+		Matrix44f toGLMatrix() const {
+			float x = (*this)[0];
+			float y = (*this)[1];
+			float z = (*this)[2];
+			float w = (*this)[3];
+			return Matrix44f (
+					1 - 2*y*y - 2*z*z,
+					2*x*y + 2*w*z,
+					2*x*y - 2*w*y,
+					0.f,
+
+					2*x*y - 2*w*z,
+					1 - 2*x*x - 2*z*z,
+					2*y*z + 2*w*x,
+					0.f,
+
+					2*x*z + 2*w*y,
+					2*y*z - 2*w*x,
+					1 - 2*x*x - 2*y*y,
+					0.f,
+					
+					0.f,
+					0.f,
+					0.f,
+					1.f);
+		}
+};
 
 /* _SIMPLEMATHGL_H_ */
 #endif
--- tests/BoneAnimationTrackTests.cc
+++ tests/BoneAnimationTrackTests.cc
@@ -1,6 +1,9 @@
 #include <UnitTest++.h>
 
 #include "ModelData.h"
+#include "SimpleMathGL.h"
+
+#include <iostream>
 
 using namespace std;
 
@@ -77,5 +80,56 @@ TEST ( BoneAnimationTrackInterpolationSimple ) {
 	CHECK_EQUAL (Vector3f (0.f, 0.f, 0.f), interpolated.translation);
 	CHECK_EQUAL (Vector3f (0.f, 0.f, 0.f), interpolated.rotation_ZYXeuler);
 	CHECK_EQUAL (Vector3f (1.f, 1.f, 1.f), interpolated.scaling);
+
+}
+
+TEST ( QuaternionMultiplicationTest ) {
+
+	// X rotation
+	smQuaternion quat_rot_quat (smQuaternion::fromGLRotate(90.f, 1.f, 0.f, 0.f));
+	Matrix44f quat_rot_matrix = quat_rot_quat.toGLMatrix();
+	CHECK_ARRAY_CLOSE(smRotate (90.f, 1.f, 0.f, 0.f).data(), quat_rot_matrix.data(), 16, 1.0e-6);
+
+	// Y rotation
+	quat_rot_quat = smQuaternion::fromGLRotate(90.f, 0.f, 1.f, 0.f);
+	quat_rot_matrix = quat_rot_quat.toGLMatrix();
+	CHECK_ARRAY_CLOSE(smRotate (90.f, 0.f, 1.f, 0.f).data(), quat_rot_matrix.data(), 16, 1.0e-6);
+
+	// Z rotation
+	quat_rot_quat = smQuaternion::fromGLRotate(90.f, 0.f, 0.f, 1.f);
+	quat_rot_matrix = quat_rot_quat.toGLMatrix();
+	CHECK_ARRAY_CLOSE(smRotate (90.f, 0.f, 0.f, 1.f).data(), quat_rot_matrix.data(), 16, 1.0e-6);
+
+	// concatenation
+	quat_rot_quat = smQuaternion::fromGLRotate (10.f, 1.f, 0.f, 0.f) 
+		* smQuaternion::fromGLRotate (-30.f, 0.f, 1.f, 0.f) 
+		* smQuaternion::fromGLRotate (200.f, 0.f, 0.f, 1.f);
+	quat_rot_matrix = quat_rot_quat.toGLMatrix();
+
+	Matrix44f rot_matrix = smRotate (10.f, 1.f, 0.f, 0.f)
+		* smRotate (-30.f, 0.f, 1.f, 0.f)
+		* smRotate (200.f, 0.f, 0.f, 1.f);
+
+	cout << "quat: " << endl << (
+			smQuaternion::fromGLRotate (10.f, 1.f, 0.f, 0.f) 
+			* smQuaternion::fromGLRotate (-30.f, 0.f, 1.f, 0.f)
+			).toGLMatrix() << endl;
+	
+	cout << "matr: " << endl << 
+		smRotate (10.f, 1.f, 0.f, 0.f) 
+		* smRotate (-30.f, 0.f, 1.f, 0.f) 
+		<< endl;
+
+	CHECK_ARRAY_CLOSE (rot_matrix.data(), quat_rot_matrix.data(), 16, 1.0e-5);
+
+	/*
+	cout << q3.transpose() << endl;	
+
+	cout << q1.toGLMatrix() << endl;
+
+	cout << qgl.toGLMatrix() << endl;
+
+	cout << smRotate (90.f, 1.f, 0.f, 0.f) << endl;
+	*/
 
 }
