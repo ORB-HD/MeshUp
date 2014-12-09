32de788bb09d0f28cb3fd4ec583c72c9f2759856
Martin Felis <martin.felis@iwr.uni-heidelberg.de>
2014-12-09 16:16:00 +0100

    drawing multiple models separated by 1m
    --HG--
    branch : development

--- src/Scene.cc
+++ src/Scene.cc
@@ -2,6 +2,7 @@
 
 #include "Model.h"
 #include "Animation.h"
+#include "GL/glew.h"
 
 void Scene::setCurrentTime (double t){
 	current_time = t;
@@ -12,32 +13,67 @@ void Scene::setCurrentTime (double t){
 }
 
 void Scene::drawMeshes(){
+	glPushMatrix();
+	glTranslatef (0., 0., 0.5 * models.size());
+
 	for (unsigned int i = 0; i < models.size(); i++) {
+
+		glTranslatef (0., 0., -1.);
 		models[i]->draw();
 	}
+
+	glPopMatrix();
 }
 
 void Scene::drawBaseFrameAxes(){
+	glPushMatrix();
+	glTranslatef (0., 0., 0.5 * models.size());
+
 	for (unsigned int i = 0; i < models.size(); i++) {
+
+		glTranslatef (0., 0., -1.);
 		models[i]->drawBaseFrameAxes();
 	}
+
+	glPopMatrix();
 }
 
 void Scene::drawFrameAxes(){
+	glPushMatrix();
+	glTranslatef (0., 0., 0.5 * models.size());
+
 	for (unsigned int i = 0; i < models.size(); i++) {
+
+		glTranslatef (0., 0., -1.);
 		models[i]->drawFrameAxes();
 	}
+
+	glPopMatrix();
 }
 
 void Scene::drawPoints(){
+	glPushMatrix();
+	glTranslatef (0., 0., 0.5 * models.size());
+
 	for (unsigned int i = 0; i < models.size(); i++) {
+
+		glTranslatef (0., 0., -1.);
 		models[i]->drawPoints();
 	}
+
+	glPopMatrix();
 }
 
 void Scene::drawCurves(){
+	glPushMatrix();
+	glTranslatef (0., 0., 0.5 * models.size());
+
 	for (unsigned int i = 0; i < models.size(); i++) {
+
+		glTranslatef (0., 0., -1.);
 		models[i]->drawCurves();
 	}
+
+	glPopMatrix();
 }
 
