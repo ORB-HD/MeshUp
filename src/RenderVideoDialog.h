1b881274e06bfccc76e844fe9b6a90d5b1997a6f
Felix5721 <judge@felixrichter.tech>
2018-09-14 14:11:01 +0200

    added video width and height validation

--- src/RenderVideoDialog.h
+++ src/RenderVideoDialog.h
@@ -5,16 +5,41 @@
 #ifndef RENDERVIDEODIALOG_H
 #define RENDERVIDEODIALOG_H
 
+#include <QValidator>
+#include <QSpinBox>
+#include <QAbstractButton>
+
 class RenderVideoDialog;
 
 #include "ui_RenderVideoDialog.h"
 
+
 class RenderVideoDialog : public QDialog, public Ui::RenderVideoDialog {
     Q_OBJECT
 
-public:
-    RenderVideoDialog (QWidget *parent = 0) {
-			   setupUi(this);
+	public:
+	    RenderVideoDialog (QWidget *parent = 0) {
+			setupUi(this);
+			connect(HeightSpinBox, SIGNAL(valueChanged(int)), this, SLOT(checkValues()));
+			connect(WidthSpinBox, SIGNAL(valueChanged(int)), this, SLOT(checkValues()));
+		}
+
+	public slots:
+		void checkValues() {
+			QAbstractButton *accept_but;
+			for (int i=0; i < buttonBox->buttons().size(); i++) {
+				if ( buttonBox->buttonRole(buttonBox->buttons().at(i)) == QDialogButtonBox::AcceptRole ) {
+					accept_but = buttonBox->buttons().at(i);
+					break;
+				}
+			}
+			if ( HeightSpinBox->value() % 8 != 0 || WidthSpinBox->value() % 8 != 0 ) {
+				accept_but->setEnabled(false);
+				warningLabel->setText("The width and height of the video need to be devisable by 8!");
+			} else {
+				accept_but->setEnabled(true);
+				warningLabel->setText("");
+			}
 		}
 
 };
